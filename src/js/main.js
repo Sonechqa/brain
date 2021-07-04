391a6154fb0dc28bf38f9e5588f6543bddb3f8a0
Sonechqa <sona.simonova@list.ru>
2021-07-04 20:03:31 +0300

    Адаптив

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,10 @@
 document.querySelectorAll('.popular__course').forEach(i => {
     i.addEventListener('click', (e) => i.classList.toggle('popular__course--active'))
 })
+
+const burger = document.querySelector('.burger')
+const wrapper = document.querySelector('.header__wrapper')
+burger.addEventListener('click', function() { 
+    burger.classList.toggle('burger--active');
+    wrapper.classList.toggle('header__wrapper--active')
+})
